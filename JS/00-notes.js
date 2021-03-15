96cafeba5552a2c0e67fa3951a42c31c91fffd7d
RutaMat <79446887+RutaMat@users.noreply.github.com>
2021-03-15 15:18:09 +0200

    Intro

---
+++ JS/00-notes.js
@@ -0,0 +1,13 @@
+//kintamieji -jo atmintyje yra laikomi informacija
+//salygos sakiniai - issakoti logikac pagal ka daryti kai daugiau/nezinau, tiea/netiesa
+//ciklai homogeniai sarasai
+//funkcijos- logika kuria pasinaudojama n kartu su skirtingais parametrais tais pačiais procesais
+//ebent'ai skirta stebeti ivykius (click, scroll, keypress ir t.t.) i kuriuos reikia sureaguoti iskvieciant norima funkcija
+
+/*
+Kintamuju iniciavimo budai:
+const- kintamasis, kurio reikšmės negali iš naujo priskirtii
+let-kintamasis, kurio reikšmes gali būti priskirta neribojama (iš naujo)
+var-varieble 
+* - yra papildomų sąlygų su kintamaisiais
+*/
---
+++ JS/01-variables.js
@@ -0,0 +1,50 @@
+console.log(3.45);
+console.log("Labas");
+console.log("rytas");
+console.log(555);
+console.log('Mano vardas yra');
+
+
+const vardas = "Ruta";
+console.log(vardas)
+
+const pasisveikinimas ='Labas rytas, Lietuva! 🟨🟩🟥';
+console.log(pasisveikinimas);
+
+const pirmasskaicius = 7;
+const antrasskaicius = 77;
+const suma = pirmasskaicius + antrasskaicius;
+
+
+console.log(pirmasskaicius, '+', antrasskaicius, '=', suma)
+
+const mathText=pirmasskaicius + '+';
+console.log(mathText);
+
+console.log('......');
+
+const petrasName = 'Petras';
+const age = 99;
+
+//Petras age is 99
+const petrasAge = petrasName + ' age is ' + age +'.';
+console.log(petrasAge);
+console.log('......');
+
+//sarasas (array)
+const pazymiai = [5, 7, 8, 10];
+console.log(pazymiai);
+
+const abc = ['a', 'b', 'c', 'd'];
+console.log(abc);
+
+//Pazymiu suma: 20
+const pazymiuSuma= pazymiai[0] + pazymiai[1] + pazymiai[2]+ pazymiai[3];
+console.log('Pazymiu suma:', pazymiuSuma);
+
+const pazymiuKiekis = pazymiai.length;
+const pazymiuVidurkis=pazymiuSuma / pazymiuKiekis;
+
+console.log('Pazymiu suma',pazymiuSuma);
+console.log('Pazymiu kiekis', pazymiuKiekis);
+console.log('Pazymiu vidurkis', pazymiuVidurkis);
