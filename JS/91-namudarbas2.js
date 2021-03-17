1c02c8e06c265cf19c64a6e03d0b1567d0954c5c
RutaMat <79446887+RutaMat@users.noreply.github.com>
2021-03-17 10:02:35 +0200

    Namu darbai2

---
+++ JS/91-namudarbas2.js
@@ -0,0 +1,132 @@
+/*
+Kintamųjų palyginimas
+Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.
+
+
+
+
+Išvesti sąrašo tipo kintamųjų ilgius
+Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
+kuris didesnis
+kuris mažesnis
+ar jie lygūs
+ar jie nelygūs
+kuris didesnis arba lygus
+kuris mažesnis arba lygus
+*/
+
+const a = 1;
+const b = 5;
+
+if (a > b) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****");  
+
+if (a < b) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****");  
+
+if (a == b) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****");  
+
+if (a != b) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****");  
+
+if (a <= b) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****");  
+
+if (a >= b) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+
+const noras = 'pomidoras';
+const norasIlgis = noras.length;
+
+const bandykite = 'Bandykite kitą kartą.'
+const bandykiteIlgis = bandykite.length;
+
+//console.log(norasIlgis);
+//console.log(bandykiteIlgis);
+
+if (norasIlgis > bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+if (norasIlgis < bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+if (norasIlgis > bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+if (norasIlgis == bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+if (norasIlgis != bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+if (norasIlgis <= bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+if (norasIlgis >= bandykiteIlgis) {
+    console.log( "Pomidoras");
+} else {
+    console.log("Bandykite kitą kartą.");
+}
+console.log("****"); 
+const marks = [10, 5, 6, 2, 8];
+array.length =
+
+const markSentence1 = `Pirmas pazymys: ${marks[0]}.`;
+console.log(markSentence1)
+
+const markSentence2 = `Antras pazymys: ${marks[1]}.`;
+console.log(markSentence2)
+
+const markSentence3 = `Trecias pazymys: ${marks[2]}.`;
+console.log(markSentence3)
+
+const markSentence4 = `Ketvirtas pazymys: ${marks[3]}.`;
+console.log(markSentence4)
+
+const markSentence5 = `Penktas pazymys: ${marks[4]}.`;
+console.log(markSentence5)
+
+
